bd93ca33689a4fd85a8c82789bbbe8d046475e6b
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2024-12-15 16:32:32 +0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,6 +1,6 @@
 -- Example
 local NovaLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/NovaLib./refs/heads/main/NovaLib.lua"))()
 
-local ui = NovaLib:CreateGui()
+local ui = NovaLib:CreateWindow("Window")
 local window = NovaLib:CreateFrame(ui)
 local tabHolder, tabs = NovaLib:CreateTabs(window, {"Tab1", "Tab2", "Tab3"})
