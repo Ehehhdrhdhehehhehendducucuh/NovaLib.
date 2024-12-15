b57c2c275f62ff3d1fa4bca788877505dee4fec8
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2024-12-15 16:46:20 +0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,6 +1,18 @@
--- Example
+-- example!
 local NovaLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/NovaLib./refs/heads/main/NovaLib.lua"))()
 
-local ui = NovaLib:CreateWindow("Window")
+local ui = NovaLib:CreateWindow("My Window")
 local window = NovaLib:CreateFrame(ui)
-local tabHolder, tabs = NovaLib:CreateTabs(window, {"Tab1", "Tab2", "Tab3"})
+local tabHolder, tabs = NovaLib:CreateTabs(window, {"Tab1", "Tab2"})
+
+NovaLib:CreateButton(window, "Click Me", function()
+    print("Button was clicked!")
+end)
+
+NovaLib:CreateToggle(window, "Enable Feature", function(isToggled)
+    print("Toggle State: ", isToggled)
+end)
+
+NovaLib:CreateSlider(window, "Volume", 0, 100, function(value)
+    print("Slider Value: ", value)
+end)
