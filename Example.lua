c0336514181a3dc3a1424fbb67774d978fdc30b7
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2024-12-17 17:31:47 +0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,9 +1,9 @@
 -- example
-local Novalib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/NovaLib./refs/heads/main/NovaLib.lua%20(3).txt"))() -- library
+local NovaLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/NovaLib./refs/heads/main/Protected_7256673614060864.txt"))() -- library
 
-Novalib:CreateLoadingScreen({
+NovaLib:CreateLoadingScreen({
     Name = "NovaWelcome",
-    Text = "Welcome to NovaHub!",
+    Text = "Welcome to NovaHub!, and enjoy",
     BackgroundColor = Color3.fromRGB(44, 47, 232),
     TextColor = Color3.fromRGB(255, 255, 255),
     TypeSpeed = 0.05
